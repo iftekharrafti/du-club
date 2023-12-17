1f6f1a38464c0b2332a65a26c77a08a8405c1fd4
Iftekhar Rafti <iftekharrafti7040@gmail.com>
2023-12-17 23:22:31 +0600

    [HALF FINISH] Dashboard home page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
-import useFetch from "@/hooks/useFetch";
 import { Col, Container, Row, Table, Card, Button } from "react-bootstrap";
 import Style from "@/styles/dashboard/dashboard.module.css";
 import { useRouter } from "next/router";
@@ -8,34 +7,98 @@ import DashboardLeftSide from "@/components/dashboard/dashboardLeftSide/Dashboar
 import LoadingSpinner from "@/components/loadingSpinner/LoadingSpinner";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Cookies from "js-cookie";
+import { subDays, format } from "date-fns";
+import Swal from 'sweetalert2'
 
-async function getData() {
-  let res = await fetch(
-    "https://dhakauniversityclub.com/api/getProductByDay?dayName=sat"
-  );
-  let json = await res.json();
-  return json;
+function getCurrentDayName() {
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const now = new Date();
+  const dayIndex = now.getDay();
+  return daysOfWeek[dayIndex];
 }
 
+
 export default function Dashboard({ data }) {
-  const [info, setInfo] = useState([]);
-  const [counter, setCounter] = useState(0);
+  const [counters, setCounters] = useState({});
 
-  useEffect(() => {
-    if (data) {
-      console.log(data);
-    }
-  }, [data]);
+  const myId = Cookies.get("USER_ID");
+  const toDate = format(new Date(), "yyyy-MM-dd");
 
-  // Increment value
-  const increment = () => {
-    setCounter(counter + 1);
+  const increment = (id) => {
+    console.log(id);
+    setCounters((prevCounters) => ({
+      ...prevCounters,
+      [id]: (prevCounters[id] || 0) + 1,
+    }));
   };
-  const decrement = () => {
-    if (counter > 1) {
-      setCounter(counter - 1);
+
+  const decrement = (id) => {
+    if (counters[id] && counters[id] > 1) {
+      setCounters((prevCounters) => ({
+        ...prevCounters,
+        [id]: prevCounters[id] - 1,
+      }));
     }
   };
+
+  const handleCartClick = async (id, price, quantity) => {
+    try {
+      const result = await Swal.fire({
+        title: "Are you sure?",
+        text: "You want to place the order",
+        icon: "success",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, Placed!",
+      });
+  
+      if (result.isConfirmed) {
+        const formData = new FormData();
+        formData.append("customer", myId);
+        formData.append("warehouseID", "4");
+        formData.append("saleDate", toDate);
+        formData.append("invoiceDate", toDate);
+        formData.append("subTotal", "6.0");
+        formData.append("vat", "0");
+        formData.append("vatPertan", "0");
+        formData.append("discountType", "0");
+        formData.append("discountPercent", "");
+        formData.append("discount", "0");
+        formData.append("totalAmount", "6.00");
+        formData.append("r1", "3");
+        formData.append("extra_sms", "0");
+        formData.append("submitBtn", "");
+        formData.append("priority", '["1"]');
+        formData.append("qty", `[${quantity}]`);
+        formData.append("productID", `[${id}]`);
+        formData.append("price", `[${price}]`);
+  
+        const response = await axios.post(
+          "https://www.dhakauniversityclub.com/api/salesStore",
+          formData,
+          {
+            headers: {
+              "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+            },
+            withCredentials: true, // Add this line
+          }
+        );
+  
+        console.log(response);
+  
+        Swal.fire({
+          title: "Order Placed!",
+          text: "Your order has been placed successfully.",
+          icon: "success",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
 
   return (
     <>
@@ -75,25 +138,29 @@ export default function Dashboard({ data }) {
                                 className="mb-3"
                                 style={{ fontSize: "15px", color: "#0B5ED7" }}
                               >
-                                ৳ 10
+                                ৳ {item?.productPrice}
                               </Card.Subtitle>
                               <div className={Style.wrapper}>
                                 <span
                                   className={Style.minus}
-                                  onClick={decrement}
+                                  onClick={() => decrement(item.productID)}
                                 >
                                   -
                                 </span>
-                                <span className={Style.num}>{counter}</span>
+                                <span className={Style.num}>
+                                  {counters[item.productID] || 0}
+                                </span>
                                 <span
                                   className={Style.plus}
-                                  onClick={increment}
+                                  onClick={() => increment(item.productID)}
                                 >
                                   +
                                 </span>
                               </div>
                               <div className="d-flex justify-content-center">
-                                <Button size="sm">Add to cart</Button>
+                                <Button size="sm" onClick={() => handleCartClick(item.productID, item?.productPrice, counters[item.productID] )}>
+                                  Add to cart
+                                </Button>
                               </div>
                             </Card.Body>
                           </div>
@@ -113,8 +180,9 @@ export default function Dashboard({ data }) {
 
 export const getStaticProps = async () => {
   try {
+    const dayName = getCurrentDayName();
     const response = await axios.get(
-      "https://dhakauniversityclub.com/api/getProductByDay?dayName=mon"
+      `https://dhakauniversityclub.com/api/getProductByDay?dayName=${dayName}`
     );
 
     const data = response.data;
